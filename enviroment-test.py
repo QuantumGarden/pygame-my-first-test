a2256c76fe5fd7cb36420c7303217b5fb21d4abe
Tomas Vicek <tomasvicek@outlook.cz>
2019-06-19 21:49:45 +0200

    classes

--- enviroment-test.py
+++ enviroment-test.py
@@ -8,13 +8,69 @@ clock = pygame.time.Clock()
 win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("First Game")
 
-x = 50
-y = 50
-width = 50
-height = 50
-obstacle_width = 50
-obstacle_height = 50
-vel = 50
+
+class player:
+    x = 50
+    y = 50
+    width = 50
+    height = 50
+    vel = 50
+
+    def __init__(self, x, y, height, width, vel):
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+        self.vel = vel
+
+    def player_movement(self, obstacles):
+
+        lower_side_collision = obstacles[px_to_grid(self.x), px_to_grid(self.y + self.height / 2 + 1)] != 0
+        left_side_collision = obstacles[px_to_grid(self.x - self.width / 2 - 1), px_to_grid(self.y)] != 0
+        right_side_collision = obstacles[px_to_grid(self.x + self.width / 2 + 1), px_to_grid(self.y)] != 0
+        upper_side_collision = obstacles[px_to_grid(self.x), px_to_grid(self.y - self.height / 2 - 1)] != 0
+
+        keys = pygame.key.get_pressed()
+
+        if keys[
+            pygame.K_LEFT] and self.x != 0 and not left_side_collision:  # Making sure the top left position of our character is greater than our vel so we never move off the screen.
+            self.x -= self.vel
+
+        if keys[
+            pygame.K_RIGHT] and self.x != 500 - self.width and not right_side_collision:  # Making sure the top right corner of our character is less than the screen width - its width
+            self.x += self.vel
+
+        if keys[pygame.K_UP] and self.y != 0 and not upper_side_collision:  # Same principles apply for the y coordinate
+            self. y -= self.vel
+
+        if keys[pygame.K_DOWN] and self.y != 500 - self.height and not lower_side_collision:
+            self.y +=self. vel
+
+        pygame.draw.rect(win, (0, 255, 255), (self.x, self.y,self.width, self.height))
+
+
+class obstacles:
+    obstacle_width = 50
+    obstacle_height = 50
+    number_of_obstacles = 5
+    obstacles = np.zeros((10, 10))
+
+    def __init__(self, obstacle_width, obstacle_height, number_of_obstacles):
+        self.obstacle_height = obstacle_height
+        self.obstacle_width = obstacle_width
+        self.number_of_obstacles = number_of_obstacles
+
+    def generate_obstacles(self):
+        for i in range(self.number_of_obstacles):
+            self.obstacles[randint(0, 9), randint(0, 9)] = 1
+
+    def generate_enviroment(self):
+        for X in range(10):
+
+            for Y in range(10):
+                if self.obstacles[X][Y] == 1:
+                    pygame.draw.rect(win, (0, 255, 0), (grid_to_px(X), grid_to_px(Y), self.obstacle_width, self.obstacle_height))
+
 
 run = True
 
@@ -32,20 +88,9 @@ def px_to_grid(x):
     return X
 
 
-def generate_enviroment():
-    for X in range(10):
-
-        for Y in range(10):
-            if obstacles[X][Y] == 1:
-                pygame.draw.rect(win, (0, 255, 0), (grid_to_px(X), grid_to_px(Y), obstacle_width, obstacle_height))
-
-
-obstacles = np.zeros((10, 10))
-obstacles[randint(0, 9), randint(0, 9)] = 1
-obstacles[randint(0, 9), randint(0, 9)] = 1
-obstacles[randint(0, 9), randint(0, 9)] = 1
-obstacles[randint(0, 9), randint(0, 9)] = 1
-print(obstacles)
+player = player(50, 50, 50, 50, 50)
+obstacles = obstacles(50, 50, 5 )
+obstacles.generate_obstacles()
 """" right_upper_corner_collision = obstacles[px_to_grid(x + width / 2), px_to_grid(y - height / 2)] != 0
     left_upper_corner_collision = obstacles[px_to_grid(x - width / 2), px_to_grid(y - height / 2)] != 0
     right_lower_corner_collision = obstacles[px_to_grid(x + width / 2), px_to_grid(y + height / 2)] != 0
@@ -53,36 +98,16 @@ print(obstacles)
     """
 while run:
 
-
-    lower_side_collision = obstacles[px_to_grid(x), px_to_grid(y + height / 2 + 1)] != 0
-    left_side_collision = obstacles[px_to_grid(x - width / 2 - 1), px_to_grid(y)] != 0
-    right_side_collision = obstacles[px_to_grid(x +width  / 2 + 1), px_to_grid(y)] != 0
-    upper_side_collision = obstacles[px_to_grid(x ), px_to_grid(y-height/2-1)] != 0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    keys = pygame.key.get_pressed()
 
- 
-    if keys[
-        pygame.K_LEFT] and x != 0 and not left_side_collision:  # Making sure the top left position of our character is greater than our vel so we never move off the screen.
-        x -= vel
+    print(player.x,player.y)
 
-    if keys[
-        pygame.K_RIGHT] and x != 500 - width and not  right_side_collision:  # Making sure the top right corner of our character is less than the screen width - its width
-        x += vel
-
-    if keys[pygame.K_UP] and y != 0 and not upper_side_collision:  # Same principles apply for the y coordinate
-        y -= vel
-
-    if keys[pygame.K_DOWN] and y != 500 - height and not lower_side_collision:
-        y += vel
-
-    # print(px_to_grid(x), px_to_grid(y))
     win.fill((0, 0, 0))
-    pygame.draw.rect(win, (0, 255, 255), (x, y, width, height))
-    generate_enviroment()
+    player.player_movement(obstacles.obstacles)
+    obstacles.generate_enviroment()
 
     pygame.display.update()
     clock.tick(10)
